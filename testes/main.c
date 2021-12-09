23370c67878210c1c6760c1c2d31168dc9466f72
kaasantos <kaioufs@gmail.com>
2021-12-09 11:17:15 -0300

    background 100%

--- testes/main.c
+++ testes/main.c
@@ -9,14 +9,9 @@ struct GameCharacter principalfrente;
 struct GameCharacter principalatras;
 struct GameCharacter principaldireita;
 struct Boneca bonecafixa; 
-struct Boneca bonecafrente;
-struct Boneca bonecatras; 
-struct GameCharacter capanga1;
-struct GameCharacter capanga2frente;
-struct GameCharacter capanga2costas;
-struct GameCharacter botfrente;
+/*struct GameCharacter botfrente;
 struct GameCharacter botatras;
-struct GameCharacter botdireita;
+struct GameCharacter botdireita; */
 
 UBYTE spritesize = 8;
 UINT8 x = 70;
@@ -46,114 +41,58 @@ void movegamecharacterboneca(struct Boneca* character, UINT8 x, UINT8 y){
 }
 
 void setupatras(){
+    INT8 z;
     principalatras.comprimento = 16;
     principalatras.largura = 16;
 
-    set_sprite_tile(0,0);
-    principalatras.spritids[0] = 0;
-    set_sprite_tile(1,1);
-    principalatras.spritids[1] = 1;
-    set_sprite_tile(2,2);
-    principalatras.spritids[2] = 2;
-    set_sprite_tile(3,3);
-    principalatras.spritids[3] = 3;
-
+    for(z = 0 ; z < 4; z++) {
+        set_sprite_tile(z,z);
+        principalatras.spritids[z] = z;
+    }
     movegamecharacter(&principalatras, x, y);
-
 }
 
 void setupfrente(){
+    INT8 z, w = 0;
     principalfrente.comprimento = 16;
     principalfrente.largura = 16;
 
-    set_sprite_tile(4,4);
-    principalfrente.spritids[0] = 4;
-    set_sprite_tile(5,5);
-    principalfrente.spritids[1] = 5;
-    set_sprite_tile(6,6);
-    principalfrente.spritids[2] = 6;
-    set_sprite_tile(7,7);
-    principalfrente.spritids[3] = 7;
+    for(z = 4; z < 8; z++) {
+        set_sprite_tile(z,z);
+        principalfrente.spritids[w] = z;
+        w++;
+    }
 }
 
 void setupdireita(){
+    INT8 z, w = 0;
     principaldireita.comprimento = 16;
     principaldireita.largura = 16;
 
-    set_sprite_tile(8,8);
-    principaldireita.spritids[0] = 8;
-    set_sprite_tile(9,9);
-    principaldireita.spritids[1] = 9;
-    set_sprite_tile(10,10);
-    principaldireita.spritids[2] = 10;
-    set_sprite_tile(11,11);
-    principaldireita.spritids[3] = 11;
-}
-
-void setupbonecafrente(){
-    bonecafrente.comprimento = 16;
-    bonecafrente.largura = 24;
-
-    set_sprite_tile(23, 23);
-    bonecafrente.spritids[0] = 24;
-    set_sprite_tile(24, 24);
-    bonecafrente.spritids[1] = 25;
-    set_sprite_tile(25, 25);
-    bonecafrente.spritids[2] = 26;
-    set_sprite_tile(26, 26);
-    bonecafrente.spritids[3] = 27;
-    set_sprite_tile(27, 27);
-    bonecafrente.spritids[4] = 28;
-    set_sprite_tile(28, 28);
-    bonecafrente.spritids[5] = 29;
-
-    //movegamecharacterboneca(&bonecafrente, 72, 27);
-}
-
-void setupbonecatras(){
-    bonecatras.comprimento = 16;
-    bonecatras.largura = 24;
-
-    set_sprite_tile(29, 29);
-    bonecatras.spritids[0] = 30;    
-    set_sprite_tile(30, 30);
-    bonecatras.spritids[1] = 31; 
-    set_sprite_tile(31, 31);
-    bonecatras.spritids[2] = 32; 
-    set_sprite_tile(32, 32);
-    bonecatras.spritids[3] = 33;
-    set_sprite_tile(27, 27);
-    bonecatras.spritids[4] = 28;
-    set_sprite_tile(28, 28);
-    bonecatras.spritids[5] = 29;
+    for(z = 8; z < 12; z++) {
+        set_sprite_tile(z,z);
+        principaldireita.spritids[w] = z;
+        w++;
+    }
 }
 
 void setupbonecafixa(){
+    INT8 z, w = 0;
     bonecafixa.comprimento = 16;
     bonecafixa.largura = 24;
 
-    set_sprite_tile(33, 23);
-    bonecafixa.spritids[0] = 24;
-    set_sprite_tile(34, 24);
-    bonecafixa.spritids[1] = 25;
-    set_sprite_tile(35, 25);
-    bonecafixa.spritids[2] = 26;
-    set_sprite_tile(36, 26);
-    bonecafixa.spritids[3] = 27;
-    set_sprite_tile(37, 27);
-    bonecafixa.spritids[4] = 28;
-    set_sprite_tile(38, 28);
-    bonecafixa.spritids[5] = 29;
-
+    for(z = 24; z < 30; z++) {
+        set_sprite_tile(z,z);
+        bonecafixa.spritids[w] = z;
+        w++;
+    }
     movegamecharacterboneca(&bonecafixa, 10, 130);
-}
+} 
 
 void setups(){
     setupfrente();
     setupatras();
     setupdireita();
-    setupbonecafrente();
-    setupbonecatras();
     setupbonecafixa();
 }
 
@@ -161,12 +100,12 @@ void main(){
     INT8 mapx = 44;
     INT8 mapy = 112;
     INT8 i;
-    set_bkg_data(0, 10, TileLabel);
+    set_bkg_data(0, 33, TileLabel);
     set_bkg_tiles(0, 0, 32, 32, mapbackground);
     scroll_bkg(mapx,mapy);
     SHOW_BKG;
 
-    set_sprite_data(0, 46, characters);
+    set_sprite_data(0, 45, characters);
     setups();
     SHOW_SPRITES;
 
@@ -197,11 +136,8 @@ void main(){
                 principaldireita.flagM = 0;
             } */
             
-            //movegamecharacter(&principaldireita, 0, 0);
-            
             movegamecharacter(&principalatras, -10, -10);
             movegamecharacter(&principalfrente, -10, -10);
-            //movegamecharacter(&principalesquerda, x, y);
             movegamecharacter(&principaldireita, x, y);
             mapx -= 1;
             scroll_bkg(-1, 0);        
@@ -214,7 +150,6 @@ void main(){
             principaldireita.spritids[3] = 11;
 
             for(i = 0; i < 4; i++) set_sprite_prop(principaldireita.spritids[i]);
-            //movegamecharacter(&principalesquerda, 0, 0);
             movegamecharacter(&principalatras, -10, -10);
             movegamecharacter(&principalfrente, -10, -10);
             movegamecharacter(&principaldireita, x, y);
@@ -223,7 +158,6 @@ void main(){
         }
         if(joypad() & J_UP){
             y -= 1;
-            //movegamecharacter(&principalesquerda, 0, 0);
             movegamecharacter(&principalfrente, -10, -10);
             movegamecharacter(&principaldireita, -10, -10);
             movegamecharacter(&principalatras, x, y);
@@ -232,7 +166,6 @@ void main(){
         }
         if(joypad() & J_DOWN){
             y += 1;
-            //movegamecharacter(&principalesquerda, 0, 0);
             movegamecharacter(&principalatras, -10, -10);
             movegamecharacter(&principaldireita, -10, -10);
             movegamecharacter(&principalfrente, x, y);
@@ -242,7 +175,9 @@ void main(){
         if(joypad() & J_A){
             printf("%u %u\n",(UINT16)(x),(UINT16)(y));
             printf("%u %u %u\n",(UINT16)mapx,(UINT16)mapy); 
-        }    
+        }
+        //if(x <= 30 & y <= 45) movegamecharacter(&capanga1, 70, 20);   
+
 
        performantdelay(6);      
     }
